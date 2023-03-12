d8f7ec78006594e835f472d412974ad02a974888
ayoub amsouri <ayoubamsouri1999@gmail.com>
2023-03-12 16:37:16 +0100

    edit an added task

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import { useState } from "react";
 function App() {
   
       const [toDo,setToDo] = useState('');
-      const [toDos,setToDos] = useState([])
+      const [toDos,setToDos] = useState([]);
+      const [editedTodo,setEditedTodo] = useState(null);
+      const [editedText,setEditedText] = useState('');
       
       const handleDelete = (id)=>{
         let updateToDos = toDos.filter(item=>item.id !== id) ;
         setToDos(updateToDos);
+      }
+
+      const switchToCompleted = (id)=>{
+        let updateToDos = toDos.map(item=>{
+          if(item.id === id){
+            item.completed = !item.completed;
+          }
+          return item;
+        })
+        setToDos(updateToDos);
+      }
+
+      const submitEdits = (id)=>{
+        toDos.map(item=>{
+          if(item.id === id){
+            item.text = editedText.trim()
+          }
+          return item;
+        });
+        setEditedTodo(null);
       }
       const handleSubmit = (e) =>{
 
@@ -28,8 +50,11 @@ function App() {
         </form>
         <div>{toDos.map(item=>(
         <div>
-        <p>{item.text}</p>
+        {item.id === editedTodo ? (<input type='text' onChange={(e)=>{setEditedText(e.target.value)}}/>) :(<p>{item.text}</p>)}
         <button onClick={()=>handleDelete(item.id)}>Delete</button>
+        <input type="checkbox" checked={item.completed} onChange={()=>switchToCompleted(item.id)} />
+        {item.id === editedTodo ?(<button onClick={()=>submitEdits(item.id)}>Submit</button>): (<button onClick={()=>setEditedTodo(item.id)}>Edit</button>) }
+        
         </div>
         ))}</div>
         </div>
